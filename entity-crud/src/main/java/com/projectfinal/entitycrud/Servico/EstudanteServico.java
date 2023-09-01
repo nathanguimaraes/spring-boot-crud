c1e3a526f1ee7d11c2e090a9c06f12a176ad099b
Nathan Guimarães <33322455+nathanguimaraes@users.noreply.github.com>
2023-09-01 09:48:53 -0300

    listando dados

--- entity-crud/src/main/java/com/projectfinal/entitycrud/Servico/EstudanteServico.java
+++ entity-crud/src/main/java/com/projectfinal/entitycrud/Servico/EstudanteServico.java
@@ -2,11 +2,17 @@ package com.projectfinal.entitycrud.Servico;
 
 import java.util.List;
 
+import java.util.Optional;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projectfinal.entitycrud.modelo.Estudante;
 import com.projectfinal.entitycrud.repositorio.EstudanteRepositorio;
+import com.projectfinal.entitycrud.excecao.EstudanteNotFoundException;
+
+
 
 
 @Service
@@ -23,7 +29,18 @@ public class EstudanteServico {
 		return estudanteRepositorio.findAll();
 	}
 	
-		
+	public Estudante buscarEstudantePorId(Long id)throws EstudanteNotFoundException {
+		Optional<Estudante> opt = estudanteRepositorio.findById(id);
+		if (opt.isPresent()) {
+			return opt.get();
+		} else {
+			throw new EstudanteNotFoundException("Estudante com id : " + id + " não existe");
+		}		
+	}	
+	
+	public void apagarEstudante(Long id) throws EstudanteNotFoundException {
+		Estudante estudante = buscarEstudantePorId(id);
+		estudanteRepositorio.delete(estudante);
+	}
 
 }
-
--- entity-crud/src/main/java/com/projectfinal/entitycrud/controle/EstudanteControle.java
+++ entity-crud/src/main/java/com/projectfinal/entitycrud/controle/EstudanteControle.java
@@ -1,5 +1,6 @@
 package com.projectfinal.entitycrud.controle;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projectfinal.entitycrud.modelo.Estudante;
 import com.projectfinal.entitycrud.Servico.EstudanteServico;
 
+import com.projectfinal.entitycrud.excecao.EstudanteNotFoundException;
+
+
+
 
 import jakarta.validation.Valid;
-
 
 @Controller
 public class EstudanteControle {
@@ -24,7 +29,9 @@ public class EstudanteControle {
 	private EstudanteServico estudanteServico;
 	
 	@GetMapping("/")
-    public String listarEstudantes() {			
+    public String listarEstudantes(Model model) {	
+		List<Estudante> estudantes = estudanteServico.buscarTodosEstudantes();
+		model.addAttribute("listaEstudantes",estudantes);
 		return "/lista-estudantes";
     }
 	
@@ -45,6 +52,17 @@ public class EstudanteControle {
 		estudanteServico.criarEstudante(estudante);
 		attributes.addFlashAttribute("mensagem", "Estudante salvo com sucesso!");
         return "redirect:/novo";
-    }	
+    }
+	
+	@GetMapping("/apagar/{id}")
+    public String apagarEstudante(@PathVariable("id") long id, RedirectAttributes attributes) {	
+		try {
+			estudanteServico.apagarEstudante(id);
+		} catch (EstudanteNotFoundException e) {
+			attributes.addFlashAttribute("mensagemErro", e.getMessage());
+		}
+        return "redirect:/";
+    }
+	
 
 }
